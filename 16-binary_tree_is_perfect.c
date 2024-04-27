7ca11d37f24ab6df4ca4587fe3a705bf9365a49a
“Sylvanus <sylvanusuzor@gmail.com>
2024-04-27 05:53:29 -0700

    16-binary_tree_is_perfect.c

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -4,18 +4,17 @@
  * binary_tree_is_perfect - Checks if a binary tree is perfect
  * @tree: Pointer to the root node of the tree to check
  *
- * Return: 1 if the tree is perfect, 0 if not
+ * Return: 1 if the tree is perfect, 0 otherwise
  */
 int binary_tree_is_perfect(const binary_tree_t *tree)
 {
-	size_t depth = binary_tree_depth(tree);
-	size_t size = binary_tree_size(tree);
-	size_t perfect_size;
+	size_t height = binary_tree_height(tree);
+	size_t nodes = binary_tree_size(tree);
 
-	if (tree == NULL)
-	return (0);
+	/* A tree is perfect if all internal nodes have two children */
+	size_t perfect_nodes = 1;
+	for (size_t i = 0; i < height; i++)
+	perfect_nodes *= 2;
 
-	perfect_size = (1 << (depth + 1)) - 1;
-
-	return (size == perfect_size);
+	return (perfect_nodes == nodes);
 }
